0379c8b670ec26893872b57fd5a28a20089b2041
leme34 <2567584274@qq.com>
2019-01-21 22:58:56 +0800

    finished

---
+++ src/main/java/com/lee/SpringbootWebsocketDemoApplication.java
@@ -0,0 +1,14 @@
+package com.lee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebsocketDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebsocketDemoApplication.class, args);
+    }
+
+}
+
